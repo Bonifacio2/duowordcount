30ab9d45ec99c5a29e2644b8a7876e7d3b481e67
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2018-08-15 18:00:14 -0300

    add actual word count

--- wordCount.js
+++ wordCount.js
@@ -4,5 +4,31 @@ let prettyGood = document.evaluate('//*[@id="root"]/div/div[2]/div/div[2]/div/di
 
 let timeToPractice = document.evaluate('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div[3]/span[1]', document, null, XPathResult.ANY_TYPE, null).iterateNext().className;
 
-let overDue = document.evaluate('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div[4]/span[1]', document, null, XPathResult.ANY_TYPE, null).iterateNext().className;
+let overdue = document.evaluate('//*[@id="root"]/div/div[2]/div/div[2]/div/div/div[4]/span[1]', document, null, XPathResult.ANY_TYPE, null).iterateNext().className;
+
+let wordTable = document.evaluate('//*[@id="root"]/div/div[2]/div/div[1]/table/tbody', document, null, XPathResult.ANY_TYPE, null).iterateNext();
+
+let wordCount = wordTable.getElementsByTagName('tr').length;
+
+let wordLevels = {
+  'strong': 0,
+  'good': 0,
+  'topractice': 0,
+  'overdue': 0
+};
+
+wordCount = 5;
+
+for(let i=0; i < wordCount; i++){
+  let wordClass = wordTable.rows[i].cells[3].getElementsByTagName('span')[0].className;
+  if (wordClass == stillStrong) {
+    wordLevels['strong'] += 1;
+  } else if (wordClass == prettyGood) {
+    wordLevels['good'] += 1;
+  } else if (wordClass == timeToPractice) {
+    wordLevels['topractice'] += 1;
+  } else {
+    wordLevels['overdue'] += 1;
+  }
+}
 
